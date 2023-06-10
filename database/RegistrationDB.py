2d230a9acfc4f24227891068551ae363ec62e5dc
unknown <jeewanthamsenanayake@gmail.com>
2023-06-10 21:41:52 +0530

    Test add

--- database/RegistrationDB.py
+++ database/RegistrationDB.py
@@ -5,4 +5,4 @@ db = firebaseCon.get_firestore_client()
 # Use db to interact with Firestore
 def setData():
     doc_ref = db.collection('collection_name').document('document_id')
-    doc_ref.set({'field1': 'dfd', 'field2': 'value2'})
+    doc_ref.set({'field1': 'dfd', 'field2': 'value2','field3': 'value3'})
