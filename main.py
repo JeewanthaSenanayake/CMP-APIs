1aced171380a0f59e565d24f1163ad1b64e7ed12
unknown <jeewanthamsenanayake@gmail.com>
2023-06-20 22:25:05 +0530

    CORS add

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = FastAPI()
 # Set up CORS
 origins = [
     "http://localhost:8080",  # Add the origins (Vue.js app URL) from where requests are allowed
+    "http://192.168.43.83:8080"
 ]
 
 app.add_middleware(
