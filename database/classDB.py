49e6b138305cd8e26ed859101cb3257b756794fc
unknown <jeewanthamsenanayake@gmail.com>
2023-06-27 21:59:35 +0530

    registration with payment col

--- database/classDB.py
+++ database/classDB.py
@@ -1,25 +1,72 @@
 import database.firebase as firebaseCon
+from datetime import date
 
 db = firebaseCon.get_firestore_client()
 
-def registerForClass(classType:str, classId:int, studentId:str):
+
+def registerForClass(classType: str, classId: int, studentId: str):
     doc_ref = db.collection('cmp').document(classType)
+    payment_ref = db.collection('payment').document(studentId)
+    today = date.today()
+    year = today.strftime("%Y")
+    # d3 = today.strftime("%Y-%B")
     try:
+        # register for class
         classData = doc_ref.get().to_dict()
         data = classData[str(classId)]
         regiList = data['regiStudents']
         regiSet = set(regiList)
         regiSet.add(studentId)
-        data['regiStudents']=list(regiSet)
-        newData= {str(classId):data}
+        data['regiStudents'] = list(regiSet)
+        newData = {str(classId): data}
         doc_ref.update(newData)
-        return "Success"
+        # payement
+        try:
+            paymentData = payment_ref.get().to_dict()
+            paymentData[classType][str(classId)] = {
+
+                f"{year}-January": False,
+                f"{year}-February": False,
+                f"{year}-March": False,
+                f"{year}-April": False,
+                f"{year}-May": False,
+                f"{year}-June": False,
+                f"{year}-July": False,
+                f"{year}-August": False,
+                f"{year}-September": False,
+                f"{year}-October": False,
+                f"{year}-November": False,
+                f"{year}-December": False
+
+            }
+            payment_ref.update(paymentData)
+            return "Success"
+        except:
+            print("Creating new document 2")
+            data = {
+                str(classId): {
+                    f"{year}-January": False,
+                    f"{year}-February": False,
+                    f"{year}-March": False,
+                    f"{year}-April": False,
+                    f"{year}-May": False,
+                    f"{year}-June": False,
+                    f"{year}-July": False,
+                    f"{year}-August": False,
+                    f"{year}-September": False,
+                    f"{year}-October": False,
+                    f"{year}-November": False,
+                    f"{year}-December": False
+                }
+            }
+            payment_ref.set({classType: data})
+            return "Success"
     except:
         print("Creating new document")
         return "Error"
-        
 
-def setClassData(classType:str, classId:int, data:dict):
+
+def setClassData(classType: str, classId: int, data: dict):
     colname = classType + 'Class'
     doc = 'class'+str(classId)
     doc_ref = db.collection(colname).document(doc)
@@ -27,35 +74,35 @@ def setClassData(classType:str, classId:int, data:dict):
         day = doc_ref.get().to_dict()
         data["day"] = day['day']
         data['classId'] = classId
-        newData = {str(day['day']):data, 'day':day['day']+1}
+        newData = {str(day['day']): data, 'day': day['day']+1}
         doc_ref.update(newData)
     except:
         print("Creating new document")
         data["day"] = 0
         data['classId'] = classId
-        doc_ref.set({"0":data, 'day':1})
+        doc_ref.set({"0": data, 'day': 1})
 
-def createClass(classType:str,data:dict):
+
+def createClass(classType: str, data: dict):
     doc_ref = db.collection('cmp').document(classType)
-    
+
     try:
         count = doc_ref.get().to_dict()
         data["id"] = count['count']
-        data["regiStudents"]=[]
-        newData = {str(count['count']):data, 'count':count['count']+1}
+        data["regiStudents"] = []
+        newData = {str(count['count']): data, 'count': count['count']+1}
         doc_ref.update(newData)
     except:
         print("Creating new document")
         data["id"] = 0
-        data["regiStudents"]=[]
-        doc_ref.set({"0":data, 'count':1})
+        data["regiStudents"] = []
+        doc_ref.set({"0": data, 'count': 1})
 
-def getAllClass(classType:str):
+
+def getAllClass(classType: str):
     doc_ref = db.collection('cmp').document(classType)
     data = doc_ref.get().to_dict()
     dataList = []
-    for i in range(0,int(data['count'])):
+    for i in range(0, int(data['count'])):
         dataList.append(data[str(i)])
     return dataList
-         
-    
