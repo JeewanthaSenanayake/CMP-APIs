c7fcb5c67744498517f71c9b6aa9c84a4a43adfa
unknown <jeewanthamsenanayake@gmail.com>
2023-07-09 10:29:11 +0530

    upload slip chnage status

--- database/paymetSlipDB.py
+++ database/paymetSlipDB.py
@@ -1,4 +1,5 @@
 import database.firebase as firebaseCon
+from datetime import datetime
 
 db = firebaseCon.get_firestore_client()
 
@@ -10,11 +11,22 @@ def uploadASlip(type:str, data:dict):
         SlipsList = allSlips["paymentSlips"]
         SlipsList.append(data)
         doc_ref.update({"paymentSlips":SlipsList})
+        
+        # Convert the string to a datetime object
+        date_object = datetime.strptime(data["month"], "%Y-%m")
+        # Convert the datetime object to the desired format
+        formatted_date = date_object.strftime("%Y-%B")
+        changePaymentStatus(data["uid"],type,data["id"],formatted_date,1)
     except:
         print("Creating new  class type")
         SlipsList = []
         SlipsList.append(data)
         doc_ref.set({"paymentSlips":SlipsList})
+        # Convert the string to a datetime object
+        date_object = datetime.strptime(data["month"], "%Y-%m")
+        # Convert the datetime object to the desired format
+        formatted_date = date_object.strftime("%Y-%B")
+        changePaymentStatus(data["uid"],type,data["id"],formatted_date,1)
 
 
 def getAllPaymentSlips(type:str):
@@ -26,3 +38,12 @@ def getAllPaymentSlips(type:str):
     except:
         emptyList = []
         return emptyList
+    
+def changePaymentStatus(uid:str,type:str,classId:int,month:str,status:int):
+    doc_ref = db.collection('payment').document(uid)
+    try:
+        allRegisterdClasss=doc_ref.get().to_dict()
+        allRegisterdClasss[type][str(classId)][month]=status
+        doc_ref.update(allRegisterdClasss)
+    except:
+        print("No data found")
